900cdbd37809ec72a944465518007dd353b2cfd5
Zain Talks Tech <zaintalkstech@gmail.com>
2021-08-14 07:58:46 +0300

    Initial Commit

---
+++ Part 2/script.js
@@ -0,0 +1,10 @@
+// Alert function: dismissable alert on the page
+alert("Subscribe!");
+
+// Confirm function: a user two options (ok, cancel) to respond to something
+confirm("Are you subscribed?");
+
+// Prompt function: it allows the user to enter data using a alert
+prompt("Are you subscribed?");
+
+// Next video: variable, and conditional statements
